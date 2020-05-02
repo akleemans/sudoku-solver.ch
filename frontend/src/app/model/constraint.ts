0742e7db278229c5acb92b7e9ce0184841723009
Adrianus Kleemans <a.kleemans@gmail.com>
2020-05-02 16:10:47 +0200

    Add constraint & constraint-type to model

---
+++ frontend/src/app/model/constraint.ts
@@ -0,0 +1,51 @@
+import {Util} from "../main/util";
+import {ConstraintType} from "./constraint-type";
+
+export class Constraint {
+  public type: ConstraintType;
+  public cellIds: number[] = [];
+  public color: string;
+
+  public constructor() {
+    this.color = Util.getRandomColor();
+  }
+
+  // Used for SINGLE_CELL_ODD_EVEN
+  public isEven: boolean;
+
+  // Used for MULTI_CELL_SUM
+  public sum: number;
+
+  // Used for TWO_CELLS_EXACT_DIFFERENCE
+  public difference: number;
+
+  // Used for TWO_CELLS_EXACT_FACTOR
+  public factor: number;
+
+  // Used for TWO_CELLS_EXACT_DIFFERENCE, TWO_CELLS_EXACT_FACTOR
+  public unknownOrder: boolean;
+
+  // Used for MULTI_CELL_PRODUCT
+  public product;
+
+  public toString(): string {
+    let cellStr = this.cellIds.map(c => c.toString()).join(', ')
+    let desc = '';
+    // TODO return a description for contraint
+    switch (this.type) {
+      case ConstraintType.SINGLE_CELL_ODD_EVEN:
+        desc = 'Cells ' + cellStr;
+        if (this.isEven) {
+          desc += ' must be even.';
+        } else {
+          desc += ' must be odd.';
+        }
+        break;
+
+      case ConstraintType.MULTI_CELL_SUM:
+        desc = 'Cells ' + cellStr + ' must add up to ' + this.sum;
+        break;
+    }
+    return desc;
+  }
+}
