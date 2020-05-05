bc831ef2083e3334fdc73c2a454fd1a7376b2df5
Adrianus Kleemans <a.kleemans@gmail.com>
2020-05-05 08:23:04 +0200

    Add tests for Util

--- frontend/src/app/main/util.spec.ts
+++ frontend/src/app/main/util.spec.ts
@@ -1,14 +1,26 @@
 import {Util} from "./util";
 
 describe('Util', () => {
-  it('should correctly count string occurences', () => {
-    expect(Util.count('4555566556', '5')).toBe(6);
-    expect(Util.count('4555566556', '4')).toBe(1);
-    expect(Util.count('4555566556', '6')).toBe(3);
+  describe('count', () => {
+    it('should correctly count string occurences', () => {
+      expect(Util.count('4555566556', '5')).toBe(6);
+      expect(Util.count('4555566556', '4')).toBe(1);
+      expect(Util.count('4555566556', '6')).toBe(3);
+    });
+
+    it('should correctly count zero occurence', () => {
+      expect(Util.count('', '1')).toBe(0);
+      expect(Util.count('23456789', '1')).toBe(0);
+    });
   });
 
-  it('should correctly count zero occurence', () => {
-    expect(Util.count('', '1')).toBe(0);
-    expect(Util.count('23456789', '1')).toBe(0);
+  describe('getRandomColor', () => {
+    it('should correctly count string occurences', () => {
+      let color = Util.getRandomColor();
+
+      expect(color.length).toBe(7);
+      expect(color.startsWith('#')).toBeTrue();
+      expect(color).not.toBe(Util.getRandomColor());
+    });
   });
 });
