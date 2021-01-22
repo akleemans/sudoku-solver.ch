07fbb934dbc65e817de229e57a36e6ee9a000595
Adrianus Kleemans <a.kleemans@gmail.com>
2021-01-22 10:06:25 +0100

    Angular post-migration stuff

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import {FormsModule} from '@angular/forms';
 import {MainComponent} from './main.component';
 
@@ -6,7 +6,7 @@ describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
